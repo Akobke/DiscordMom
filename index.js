79d0147a58c9dde8a3a28858ce531a4c8fc4125f
PJongu <69169366+Akobke@users.noreply.github.com>
2022-09-10 03:35:12 -0400

    Added

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ require('dotenv').config()
 const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
 const authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
 
-const client = require('twilio')(accountSid, authToken, {
+const twilioClient = require('twilio')(accountSid, authToken, {
     lazyLoading: true
 });
 const client = new Client({ intents:  [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-
+    GatewayIntentBits.MessageContent
     ] });
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -23,5 +23,18 @@ client.on('messageCreate', (message) =>{
     if(message.author.bot){
         return;
     }
-    message.reply("fuck you")
+    message.reply(message.content)
+    let random = Math.floor(Math.random() * 6);
+    console.log(random);
+        if(random === 2){
+        twilioClient.messages.
+        create({
+            body: message.content,
+            from: '+15737875205',
+            to: '+14344217028'
+          })
+         .then(message => console.log(message.sid));
+    }
+
+   
 })
