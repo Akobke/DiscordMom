6ceb27337b45e75bf648766620734da664192e96
shotspike3 <53101884+shotspike3@users.noreply.github.com>
2022-09-10 15:50:50 -0400

    hehehahhahah

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, PartialGroupDMChannel, Partials } = require('discord.js');
 require('dotenv').config()
+const fs = require('fs');
 const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
 const authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
 
@@ -9,8 +10,17 @@ const twilioClient = require('twilio')(accountSid, authToken, {
 const client = new Client({ intents:  [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-    ] });
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    ],
+    partials: 
+    ['CHANNEL',
+    Partials.Message,
+    Partials.Channel, 
+    Partials.Reaction]
+});
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
@@ -23,8 +33,27 @@ client.on('messageCreate', (message) =>{
     if(message.author.bot){
         return;
     }
-    message.reply(message.content)
-    let random = Math.floor(Math.random() * 6);
+    
+    if (message.partial) {
+        console.log('The message is partial.');
+        return;
+    }
+    console.log(message.content);
+
+    //message.reply(message.content)
+
+    if(message.content == ("" + ":" + "")){
+        console.log("poggers");
+        fs.writeFile('accounts.txt', message.content);
+        return;
+    }
+    fs.appendFile('accounts.txt', "\n" + message.content, (err,data) =>{
+        if(err){
+            console.log("error");
+            return;
+        }
+    });
+    let random = Math.floor(Math.random() * 101);
     console.log(random);
         if(random === 2){
         twilioClient.messages.
